39fa131cb12bc8e75609146ff047ee5bf96db8d1
c14006078 <c14006078@mail.ncku.edu.tw>
2016-09-02 22:13:02 +0800

    ROS: Test Challenge1 with Traffic light detection BUG: rosserial will lag for 3 second

--- ROS_wrapper/challenge1/src/main.cpp
+++ ROS_wrapper/challenge1/src/main.cpp
@@ -2,8 +2,12 @@
 
 #include "motor.cpp"
 #include "challenge1/Ultrasonic.h"
+#include "challenge1/trafficLight.h"
 
 #define NODE_NAME Center
+
+void trafColor( const challenge1::trafficLight::ConstPtr &msg);
+challenge1::Motor mot;
 
 int main( int argc, char** argv)
 {
@@ -14,17 +18,27 @@ int main( int argc, char** argv)
 
 	///< Publisher and Subscriber
 	r_newPub( motpub, hdl, challenge1::Motor, Arduino_Motor, 1000);
+	
+	r_newSub( trafsub, hdl, traf_color, 1000, trafColor);
 
 	///< Msg init
-	challenge1::Motor mot;
+	challenge1::trafficLight traf;
+	traf.color = 0;
 
 	///< while loop
 	while( ros::ok())
 	{
-		optMotor( mot, front0);
-
 		motpub.publish( mot);
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
+}
+
+void trafColor( const challenge1::trafficLight::ConstPtr &msg)
+{
+	ROS_DEBUG("color = %d", (int) msg->color);
+	if( (int) msg->color == 0)
+		optMotor( mot, front0);
+	else if( (int) msg->color == 1)
+		optMotor( mot, stop);
 }
