caced4ee0abeb15d7e969c830649881e3ef0afc5
CheHsuan <exas60807@gmail.com>
2016-09-05 12:24:19 +0800

    ArtNet:init the laser fort

--- ArtNet/level3/bridge.py
+++ ArtNet/level3/bridge.py
@@ -1,5 +1,44 @@
 import serial
 import time
+import tty, termios, sys
+
+# Returns a single character from standard input
+def getchar():     
+   fd = sys.stdin.fileno()
+   old_settings = termios.tcgetattr(fd)
+   try:
+      tty.setraw(sys.stdin.fileno())
+      ch = sys.stdin.read(1)
+   finally:
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+   return ch
+
+# initialize the stepper and locate the 4 corner
+def init():
+    i = 1
+    while i <= 4: 
+        ch = getchar()
+        if ch == '\x1b' and '[' == getchar():
+            ch = getchar() 
+            if ch == 'A':
+                 print 'up'
+            elif ch == 'B':
+                print 'down'
+            elif ch == 'C':
+                print 'right'
+            elif ch == 'D':
+                print 'left'
+        elif ch == ' ':
+            print 'set %d\'s corner' % i
+            #TODO transmit the message to arduino
+            i = i + 1
+        elif ch == 'q':
+            return
+        else:
+            continue
+    print 'All corners are set'
+    #TODO transmit the message to arduino
+
 # configure the serial connection
 def connect(port='/dev/ttyACM0',baudrate=115200):
     ser = serial.Serial(port, baudrate,
@@ -7,7 +46,8 @@ def connect(port='/dev/ttyACM0',baudrate=115200):
     stopbits=serial.STOPBITS_TWO,
     bytesize=serial.SEVENBITS)
     ser.isOpen()
-    print 'connection start'   
+    print 'connection start'
+    init()   
     return ser
 
 # send message to laser gun
