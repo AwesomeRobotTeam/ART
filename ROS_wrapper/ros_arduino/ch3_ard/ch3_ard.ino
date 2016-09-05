e8468e9d9608e3486233dcb7fb0c3798dfa27009
c14006078@gmail.com <c14006078@gmail.com>
2016-09-05 22:27:44 +0800

    ROS: challenge3 two stepper with lacer Arduino will block when the Stepper turn to the specify step, so we can use lacer after it in arduino.

--- ROS_wrapper/ros_arduino/ch3_ard/ch3_ard.ino
+++ ROS_wrapper/ros_arduino/ch3_ard/ch3_ard.ino
@@ -2,16 +2,21 @@
 #include <challenge3/Stepper.h>
 #include <Stepper.h>
 
+#define lacer 7
+
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 
 //Stepper Motor
 Stepper stp1(stepsPerRevolution, 5, 3, 4, 2);
-Stepper stp2(stepsPerRevolution, 5, 3, 4, 2);//
+Stepper stp2(stepsPerRevolution, 11, 9, 10, 8);
 
 void wrtStepper( const challenge3::Stepper &msg)
 {
    stp1.step( msg.rosteps1);
    stp2.step( msg.rosteps2);
+   digitalWrite( lacer, HIGH);
+   delay( 1000);
+   digitalWrite( lacer, LOW);
 }
 
 ros::NodeHandle nh;
@@ -20,11 +25,12 @@ ros::Subscriber< challenge3::Stepper > substepper("Arduino_Stepper", wrtStepper)
 
 void setup()
 {
+  pinMode( lacer, OUTPUT);
   // set the speed at 60 rpm:
   stp1.setSpeed(5);
   stp2.setSpeed(5);
   // initialize the serial port:
-  Serial.begin(57600);
+  //Serial.begin(57600);
   
   nh.initNode();
   nh.subscribe( substepper);
