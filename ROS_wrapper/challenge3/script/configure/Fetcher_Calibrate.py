0e0ca0834e34d58762a31c3cdc407fed8ca43dcf
CheHsuan <exas60807@gmail.com>
2016-09-17 19:43:34 +0800

    ArtNet:image fetcher calibrate

---
+++ ROS_wrapper/challenge3/script/configure/Fetcher_Calibrate.py
@@ -0,0 +1,34 @@
+# Example for Fetcher.cpp
+import sys
+import cv2
+import fetcher
+import signal
+
+key = 1
+def handler(signal, frame):
+    global key
+    key = 27
+    print "key %d" % key
+
+if len(sys.argv) < 3:
+    print "need argument for video and blocksize"
+    sys.exit()
+cap = cv2.VideoCapture(int(sys.argv[1]))
+myFetcher = fetcher.Fetcher()
+signal.signal(signal.SIGTSTP, handler)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    print key
+    cv2.waitKey(1)
+
+    # Our operations on the frame come here
+    fileName = myFetcher.calibrate(frame, int(sys.argv[2]), 200, key)
+    if not fileName == "":
+	    print fileName
+	    break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
