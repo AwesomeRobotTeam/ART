e14e19aa586e2d93ba0d4d6cf1a37afacd053c37
CheHsuan <exas60807@gmail.com>
2016-09-19 13:38:38 +0800

    ArtNet:only mkdir if the arg of save video image is set

--- ROS_wrapper/challenge3/script/module/SaveRealImage.py
+++ ROS_wrapper/challenge3/script/module/SaveRealImage.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+import time
+import random
 
 catagory = 10
 
@@ -8,14 +10,13 @@ catagory_dir = ['airplane/','automobile/','bird/','cat/','deer/','dog/','frog/',
 catagory_name = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 catagory_num = [23, 3, 23, 4, 20, 30, 40, 50, 60, 3]
 
-if not os.path.isdir(image_root):
-    os.mkdir(image_root)
-
 def saveImage(img_list):
     count = 0 
+    if not os.path.isdir(image_root):
+        os.mkdir(image_root)
     for i in range(catagory):
         if not os.path.isdir(image_root + catagory_dir[i]):
             os.mkdir(image_root + catagory_dir[i])
         for index, img in enumerate(img_list[count:count+catagory_num[i]]):
-            cv2.imwrite(image_root + catagory_dir[i] + catagory_name[i] + str(index) + ".jpg", img)  
+            cv2.imwrite(image_root + catagory_dir[i] + catagory_name[i] + str(index)+ "_" + time.strftime("%d%H%M%S")+ ".jpg", img)  
         count = count + catagory_num[i]
