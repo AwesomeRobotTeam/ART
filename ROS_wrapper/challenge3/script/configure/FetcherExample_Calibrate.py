a5efc5a81736f86f187d2d6e3ab56490f37b21f0
CheHsuan <exas60807@gmail.com>
2016-09-21 02:32:43 +0800

    ArtNet:use mouse click for calibration

--- ROS_wrapper/challenge3/script/configure/FetcherExample_Calibrate.py
+++ ROS_wrapper/challenge3/script/configure/FetcherExample_Calibrate.py
@@ -1,24 +1,39 @@
 # Example for Fetcher.cpp
-import sys
+
 import cv2
 import fetcher
+import sys 
 import signal
+
+if len(sys.argv) < 3:
+    print "need argument for video and blocksize"
+    sys.exit()
 
 key = 1
 def handler(signal, frame):
     global key
     key = 27
 
-if len(sys.argv) < 3:
-    print "need argument for video and blocksize"
-    sys.exit()
-cap = cv2.VideoCapture(int(sys.argv[1]))
 myFetcher = fetcher.Fetcher()
+
+ptNumber = 0
+def onMouse(event, x, y, flags, param):
+    global ptNumber
+
+    if event == cv2.EVENT_LBUTTONUP:
+        myFetcher.setPt(x, y, ptNumber)
+	ptNumber = ptNumber + 1
+
+cv2.namedWindow("Video Captured")
+cv2.setMouseCallback("Video Captured", onMouse)
+cap = cv2.VideoCapture(int(sys.argv[1]))
+
 signal.signal(signal.SIGTSTP, handler)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
     cv2.waitKey(1)
 
     # Our operations on the frame come here
@@ -26,6 +41,8 @@ while(True):
     if not fileName == "":
 	    print fileName
 	    break
+
+    cv2.imshow("Video Captured", frame)
 
 # When everything done, release the capture
 cap.release()
