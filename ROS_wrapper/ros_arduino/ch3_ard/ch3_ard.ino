79f880a6b633f1432e2fc54a1c145f35b535b8de
CheHsuan <exas60807@gmail.com>
2016-09-20 18:50:38 +0800

    update by 87

--- ROS_wrapper/ros_arduino/ch3_ard/ch3_ard.ino
+++ ROS_wrapper/ros_arduino/ch3_ard/ch3_ard.ino
@@ -13,9 +13,9 @@ Stepper stp2(stepsPerRevolution, 11, 9, 10, 8);
 void wrtFort( const challenge3::Laser_fort &msg)
 {
    stp1.step( msg.rotsteps1);
-   delay( 100);
+   //delay( 100);
    stp2.step( msg.rotsteps2);
-   delay( 100);
+   //delay( 100);
    digitalWrite( lacer, msg.hit);
    if( msg.delay != 0)
      delay( msg.delay);
@@ -29,8 +29,8 @@ void setup()
 {
   pinMode( lacer, OUTPUT);
   // set the speed at 60 rpm:
-  //stp1.setSpeed(5);
-  //stp2.setSpeed(5);
+  stp1.setSpeed(5);
+  stp2.setSpeed(5);
   // initialize the serial port:
   //Serial.begin(57600);
   
