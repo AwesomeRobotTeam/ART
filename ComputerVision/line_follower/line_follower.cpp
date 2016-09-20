92a485b03184fe38d4c01406cbe6114ee6210a63
GuanBiao <xup6ej04jp6@gmail.com>
2016-09-20 17:29:54 +0800

    CV: Modify the algorithm

--- ComputerVision/line_follower/line_follower.cpp
+++ ComputerVision/line_follower/line_follower.cpp
@@ -1,5 +1,5 @@
 /*
- * Preferred parameters: -v 0 -s 3 -w 200 -h 50 -g 10 -t 20
+ * Preferred parameters: -v 0 -s 3 -w 125 -h 100 -g 5 -r 2 -t 20
  *
  * By Guan-Wen Lin
  * Last modified: Sep 20, 2016.
@@ -16,33 +16,29 @@
 #define TURN_LEFT   1
 #define TURN_RIGHT  2
 
-cv::Mat voteMovingDirection(cv::Mat segmentedInputFrame, int &votes_left, int &votes_middle, int &votes_right, cv::Size windowSize, int windowGap) {
+cv::Mat voteMovingDirection(cv::Mat segmentedInputFrame, int &votes_left, int &votes_middle, int &votes_right, int windowWidth, int windowHeight, int windowGap, int windowRatio) {
 	cv::Point imgCenter(segmentedInputFrame.cols / 2, segmentedInputFrame.rows / 2);
-	cv::Point windowOrigin_left  (imgCenter.x - 3 * windowSize.width / 2 - windowGap, imgCenter.y - windowSize.height / 2);
-	cv::Point windowOrigin_middle(imgCenter.x -     windowSize.width / 2            , imgCenter.y - windowSize.height / 2);
-	cv::Point windowOrigin_right (imgCenter.x +     windowSize.width / 2 + windowGap, imgCenter.y - windowSize.height / 2);
+	cv::Point windowOrigin_left  (imgCenter.x - (2 * windowRatio + 1) * windowWidth / 2 - windowGap, imgCenter.y - windowHeight / 2);
+	cv::Point windowOrigin_middle(imgCenter.x -                         windowWidth / 2            , imgCenter.y - windowHeight / 2);
+	cv::Point windowOrigin_right (imgCenter.x +                         windowWidth / 2 + windowGap, imgCenter.y - windowHeight / 2);
 
-	cv::Rect window_left  (windowOrigin_left  , windowSize);
-	cv::Rect window_middle(windowOrigin_middle, windowSize);
-	cv::Rect window_right (windowOrigin_right , windowSize);
+	cv::Rect window_left  (windowOrigin_left  , cv::Size(windowWidth * windowRatio, windowHeight));
+	cv::Rect window_middle(windowOrigin_middle, cv::Size(windowWidth              , windowHeight));
+	cv::Rect window_right (windowOrigin_right , cv::Size(windowWidth * windowRatio, windowHeight));
 
-	cv::Point center_left(0, 0), center_middle(0, 0), center_right(0, 0);
-	int ptCount_left = 0, ptCount_middle = 0, ptCount_right = 0;
+	cv::Point center_left(0, 0), center_right(0, 0), center_avg(0, 0);
+	int ptCount_left = 0, ptCount_right = 0;
 
 	cv::Mat edges;
 	cv::Canny(segmentedInputFrame, edges, 50, 200);
 
 	for (int i = 0; i < edges.cols; i++) {
-		for (int j = windowOrigin_left.y; j < windowOrigin_left.y + windowSize.height; j++) {
+		for (int j = windowOrigin_left.y; j < windowOrigin_left.y + windowHeight; j++) {
 			if (edges.at<uchar>(j, i) > 0) {
 				cv::Point pt(i, j);
 				if (window_left.contains(pt)) {
 					center_left += pt;
 					ptCount_left++;
-				}
-				else if (window_middle.contains(pt)) {
-					center_middle += pt;
-					ptCount_middle++;
 				}
 				else if (window_right.contains(pt)) {
 					center_right += pt;
@@ -52,14 +48,19 @@ cv::Mat voteMovingDirection(cv::Mat segmentedInputFrame, int &votes_left, int &v
 		}
 	}
 
-	center_left   = ptCount_left   > 0 ? cv::Point(center_left.x   / ptCount_left  , center_left.y   / ptCount_left)   : cv::Point(0, 0);
-	center_middle = ptCount_middle > 0 ? cv::Point(center_middle.x / ptCount_middle, center_middle.y / ptCount_middle) : cv::Point(0, 0);
-	center_right  = ptCount_right  > 0 ? cv::Point(center_right.x  / ptCount_right , center_right.y  / ptCount_right)  : cv::Point(0, 0);
+	center_left  = ptCount_left  > 0 ? cv::Point(center_left.x  / ptCount_left , center_left.y  / ptCount_left)  : cv::Point(0, 0);
+	center_right = ptCount_right > 0 ? cv::Point(center_right.x / ptCount_right, center_right.y / ptCount_right) : cv::Point(0, 0);
 
-	votes_left   = window_left.contains(center_left)     ? votes_left   + 1 : votes_left   + 0;
-	votes_middle = window_middle.contains(center_middle) ? votes_middle + 1 : votes_middle + 0;
-	votes_right  = window_right.contains(center_right)   ? votes_right  + 1 : votes_right  + 0;
+	if      (ptCount_left > 0  && ptCount_right > 0)  center_avg = cv::Point((center_left.x + center_right.x) / 2, (center_left.y + center_right.y) / 2);
+	else if (ptCount_left == 0 && ptCount_right > 0)  center_avg = center_right;
+	else if (ptCount_left > 0  && ptCount_right == 0) center_avg = center_left;
+	else                                              center_avg = imgCenter;
 
+	votes_left   = window_left.contains(center_avg)   ? votes_left   + 1 : votes_left   + 0;
+	votes_middle = window_middle.contains(center_avg) ? votes_middle + 1 : votes_middle + 0;
+	votes_right  = window_right.contains(center_avg)  ? votes_right  + 1 : votes_right  + 0;
+
+	cv::circle(edges, center_avg, 3, cv::Scalar(255));
 	cv::rectangle(edges, window_left  , cv::Scalar(255));
 	cv::rectangle(edges, window_middle, cv::Scalar(255));
 	cv::rectangle(edges, window_right , cv::Scalar(255));
@@ -71,12 +72,16 @@ int computeMovingDirection(int preMovingDirection, int &votes_left, int &votes_m
 	int curMovingDirection;
 
 	if (votes_left > votingThreshold || votes_middle > votingThreshold || votes_right > votingThreshold) {
-		if (votes_left > votingThreshold)
-			curMovingDirection = TURN_LEFT;
+		if (votes_left > votingThreshold) {
+			if (votes_right == 0) curMovingDirection = TURN_RIGHT;
+			else                  curMovingDirection = TURN_LEFT;
+		}
 		else if (votes_middle > votingThreshold)
 			curMovingDirection = GO_STRAIGHT;
-		else if (votes_right > votingThreshold)
-			curMovingDirection = TURN_RIGHT;
+		else if (votes_right > votingThreshold) {
+			if (votes_left == 0) curMovingDirection = TURN_LEFT;
+			else                 curMovingDirection = TURN_RIGHT;
+		}
 
 		votes_left = votes_middle = votes_right = 0;
 	}
@@ -89,8 +94,8 @@ int computeMovingDirection(int preMovingDirection, int &votes_left, int &votes_m
 int main(int argc, char *argv[]) {
 	// Check command arguments
 	int c;
-	int DEVICE, SEGMENTATION_RATIO, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_GAP, VOTING_THRESHOLD;
-	while ((c = getopt(argc, argv, "v:s:w:h:g:t:")) != -1) {
+	int DEVICE, SEGMENTATION_RATIO, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_GAP, WINDOW_RATIO, VOTING_THRESHOLD;
+	while ((c = getopt(argc, argv, "v:s:w:h:g:r:t:")) != -1) {
 		switch (c) {
 			case 'v':
 				DEVICE = atoi(optarg);
@@ -107,16 +112,19 @@ int main(int argc, char *argv[]) {
 			case 'g':
 				WINDOW_GAP = atoi(optarg);
 				break;
+			case 'r':
+				WINDOW_RATIO = atoi(optarg);
+				break;
 			case 't':
 				VOTING_THRESHOLD = atoi(optarg);
 				break;
 			default: // '?'
-				std::cout << "Usage: " << argv[0] << " [-v device] [-s segmentation_ratio] [-w window_width] [-h window_height] [-g window_gap] [-t voting_threshold]" << std::endl;
+				std::cout << "Usage: " << argv[0] << " [-v device] [-s segmentation_ratio] [-w window_width] [-h window_height] [-g window_gap] [-r window_ratio] [-t voting_threshold]" << std::endl;
 				return -1;
 		}
 	}
-	if (argc != 13) {
-		std::cout << "Usage: " << argv[0] << " [-v device] [-s segmentation_ratio] [-w window_width] [-h window_height] [-g window_gap] [-t voting_threshold]" << std::endl;
+	if (argc != 15) {
+		std::cout << "Usage: " << argv[0] << " [-v device] [-s segmentation_ratio] [-w window_width] [-h window_height] [-g window_gap] [-r window_ratio] [-t voting_threshold]" << std::endl;
 		return -1;
 	}
 
@@ -141,7 +149,7 @@ int main(int argc, char *argv[]) {
 		myVideoCapture >> inputFrame;
 		inputFrame(cv::Range(inputFrame.rows - inputFrame.rows / SEGMENTATION_RATIO, inputFrame.rows), cv::Range::all()).copyTo(segmentedInputFrame);
 
-		cv::Mat edges = voteMovingDirection(segmentedInputFrame, votes_left, votes_right, votes_middle, cv::Size(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_GAP);
+		cv::Mat edges = voteMovingDirection(segmentedInputFrame, votes_left, votes_middle, votes_right, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_GAP, WINDOW_RATIO);
 
 		movingDirection = computeMovingDirection(movingDirection, votes_left, votes_middle, votes_right, VOTING_THRESHOLD);
 		std::cout << movingDirection << std::endl;
