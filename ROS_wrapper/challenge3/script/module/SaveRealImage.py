a695ee1fc8340f4eb7df65862186fb35ecc00ac1
CheHsuan <exas60807@gmail.com>
2016-09-20 12:53:16 +0800

    ArtNet:adjust the number of dog

--- ROS_wrapper/challenge3/script/module/SaveRealImage.py
+++ ROS_wrapper/challenge3/script/module/SaveRealImage.py
@@ -8,7 +8,7 @@ catagory = 10
 image_root = "./test/"
 catagory_dir = ['airplane/','automobile/','bird/','cat/','deer/','dog/','frog/','horse/','ship/','truck/']
 catagory_name = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
-catagory_num = [23, 3, 23, 4, 20, 30, 40, 50, 60, 3]
+catagory_num = [23, 3, 23, 4, 20, 31, 40, 50, 59, 3]
 
 def saveImage(img_list):
     count = 0 
