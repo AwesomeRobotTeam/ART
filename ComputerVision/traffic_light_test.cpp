1a7f268edacaa0e1315c1467d36dca5ef4e7e03b
c14006078@gmail.com <c14006078@gmail.com>
2016-09-21 12:48:20 +0800

    ROS ch1: detect traffic light improve

--- ComputerVision/traffic_light_test.cpp
+++ ComputerVision/traffic_light_test.cpp
@@ -1,7 +1,10 @@
 /*
+ * Preferred parameters: -v 0 -s 3 -r 300 -d 100 -l 2
+ *
  * By Guan-Wen Lin
- * Last modified: Sep 2, 2016.
+ * Last modified: Sep 21, 2016.
  */
+
 #include <iostream>
 #include <vector>
 #include <ctime>
@@ -10,7 +13,14 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+/*ROS*/
+#include "ros_api.h"
+#include "challenge1/trafficLight.h"
 
+#define GREEN 0
+#define RED		1
+/**/
+c
 cv::Mat extractRedHue(cv::Mat inputFrame_BGR) {
 	// Convert input image to HSV
 	cv::Mat inputFrame_HSV;
@@ -57,12 +67,15 @@ bool findBiggestRedHue(const cv::Mat &redHueImg, const int threshold_red, cv::Po
 int main(int argc, char *argv[]) {
 	// Check command arguments
 	int c;
-	int DEVICE, THRESHOLD_RED, THRESHOLD_DISTANCE;
+	int DEVICE, SEGMENTATION_RATIO, THRESHOLD_RED, THRESHOLD_DISTANCE;
 	double LATENCY;
-	while ((c = getopt(argc, argv, "v:r:d:s:")) != -1) {
+	while ((c = getopt(argc, argv, "v:s:r:d:l:")) != -1) {
 		switch (c) {
 			case 'v':
 				DEVICE = atoi(optarg);
+				break;
+			case 's':
+				SEGMENTATION_RATIO = atoi(optarg);
 				break;
 			case 'r':
 				THRESHOLD_RED = atoi(optarg);
@@ -70,19 +83,28 @@ int main(int argc, char *argv[]) {
 			case 'd':
 				THRESHOLD_DISTANCE = atoi(optarg);
 				break;
-			case 's':
+			case 'l':
 				LATENCY = atof(optarg);
 				break;
 			default: // '?'
-				std::cout << "Usage: " << argv[0] << " [-v device] [-r threshold_red] [-d threshold_distance] [-s latency]" << std::endl;
+				std::cout << "Usage: " << argv[0] << " [-v device] [-s segmentation_ratio] [-r threshold_red] [-d threshold_distance] [-l latency]" << std::endl;
 				return -1;
 		}
 	}
-	if (argc != 9) {
-		std::cout << "Usage: " << argv[0] << " [-v device] [-r threshold_red] [-d threshold_distance] [-s latency]" << std::endl;
+	if (argc != 11) {
+		std::cout << "Usage: " << argv[0] << " [-v device] [-s segmentation_ratio] [-r threshold_red] [-d threshold_distance] [-l latency]" << std::endl;
 		return -1;
 	}
 
+	/*ROS*/
+	r_init( Camera_for_Traffic_Light);
+	r_hdlr( hdl);
+
+	r_newPub( campub, hdl, challenge1::trafficLight, traf_color, 100);
+	challenge1::trafficLight traf;
+
+	ros::Rate loop_rate( 10);
+	/**/
 
 	// Start to detect the red traffic light
 	cv::VideoCapture myVideoCapture(DEVICE);
@@ -97,20 +119,26 @@ int main(int argc, char *argv[]) {
 	bool hasDetectedRedHue = false;
 	time_t start;
 	cv::Point prevCenter(-1, -1);
-	while (1) {
+	//while (1) {
+	while( ros::ok()){
 		int key = cv::waitKey(1);
 		if ((key & 0xFF) == 27) // 'Esc' key
 			return 0;
 
-		cv::Mat inputFrame_BGR;
+		cv::Mat inputFrame_BGR, segmentedInputFrame_BGR;
 		myVideoCapture >> inputFrame_BGR;
-		cv::Mat redHueImg = extractRedHue(inputFrame_BGR);
+		inputFrame_BGR(cv::Range(0, inputFrame_BGR.rows / SEGMENTATION_RATIO), cv::Range::all()).copyTo(segmentedInputFrame_BGR);
+		cv::Mat redHueImg = extractRedHue(segmentedInputFrame_BGR);
 
 		cv::Point curCenter;
 		if (findBiggestRedHue(redHueImg, THRESHOLD_RED, curCenter)) {
-			// A new incoming red object is detected
+			// A new inco( ros::ok()){ming red object is detected
 			if (!hasDetectedRedHue && cv::norm(curCenter - prevCenter) > THRESHOLD_DISTANCE) {
 				std::cout << "Wait" << std::endl;
+				/*ROS*/
+				traf.color = RED;
+				/**/
+
 				hasDetectedRedHue = true;
 				prevCenter = curCenter;
 				start = clock();
@@ -118,17 +146,31 @@ int main(int argc, char *argv[]) {
 			// If we wait too long, the red object is not the red traffic light
 			else if (((clock() - start) / (double)CLOCKS_PER_SEC) > LATENCY) {
 				std::cout << "Go" << std::endl;
+				/*ROS*/
+				traf.color = GREEN;
+				/**/
 				hasDetectedRedHue = false;
 				prevCenter = curCenter;
 			}
 			// The red object is being detected
 			else
 				std::cout << "Wait" << std::endl;
+				/*ROS*/
+				traf.color = RED;
+				/**/
 		}
-		else
+		else {
 			std::cout << "Go" << std::endl;
+			/*ROS*/
+			traf.color = GREEN;
+			/**/
 
-		cv::imshow("Video Captured", inputFrame_BGR);
+
+			hasDetectedRedHue = false;
+			prevCenter = cv::Point(-1, -1);
+		}
+
+		cv::imshow("Video Captured", segmentedInputFrame_BGR);
 		cv::imshow("Red Hue", redHueImg);
 	}
 
